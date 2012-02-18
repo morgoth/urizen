773d35e3194f8628e47e6bdabaa95b7cff58c202
Wojciech Wnętrzak <w.wnetrzak@gmail.com>
2012-02-18 14:47:04 +0100

    added sass partials

--- controller.rb
+++ controller.rb
@@ -1,2 +1,4 @@
 layout /.*html.haml/ => 'layout.html.haml'
 ignore "Gemfile", "Gemfile.lock", "credentials.yaml", "Rakefile", "README.md"
+# Ignore filenames with an underscore at the beginning:
+ignore /\/_.*/
